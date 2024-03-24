d169b8dac1fe1fc8416c97ccd0fd8aea996db06f
sai <saihaansyed2003@gmail.com>
2024-03-24 14:23:49 -0400

    initial http server setup

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+
+type person struct {
+  UUID string `json:"id"`
+  Givenname string `json:"givenname"`
+  Surname string `json:"surname"`  
+}
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+var persons = []person{
+  {UUID: "3fdvbh4578e", Givenname: "Saihaan", Surname: "Syed"},
+  {UUID: "jhb436bhfbd", Givenname: "Ish", Surname: "Dur"},
+  {UUID: "sdfgy4378wf", Givenname: "David", Surname: "Attenborough"},
+}
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+  c.IndentedJSON(http.StatusOK, albums)
+}
+func getPersons(c *gin.Context)  {
+  c.IndentedJSON(http.StatusOK, persons)
+}
+
+func main()  {
+  ginRouter := gin.Default()
+  ginRouter.GET("/persons", getPersons)
+  ginRouter.GET("/albums", getAlbums)
+  ginRouter.Run("localhost:8000")
+}
